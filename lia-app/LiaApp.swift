406052eec09de0712072e8d68ef94d9d8dead61e
Kevin Henriksson <henriksson.kevin@gmail.com>
2024-04-29 10:51:08 +0200

    Project setup (#1)

---
+++ lia-app/LiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  lia_appApp.swift
+//  lia-app
+//
+//  Created by Kevin Henriksson (External - Magnit Global Sweden II AB) on 2024-04-29.
+//
+
+import SwiftUI
+
+@main
+struct LiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
